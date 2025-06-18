586a4b469af4154e4986cfc486d50bdf65d8d35c
Serhii L. <118447093+Inanim4te@users.noreply.github.com>
2025-06-18 15:31:08 +0700

    switch to axios

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,10 +1,11 @@
 import iziToast from 'izitoast';
+import axios from 'axios';
 import 'izitoast/dist/css/iziToast.min.css';
 import { renderPics } from './render-functions';
 
 const API_KEY = '33649719-b7fecbfe979c6e7e0b54f5aa7';
 
-export function fetchPics() {
+export async function fetchPics() {
   const galleryEl = document.querySelector('.gallery');
   const loaderEl = document.querySelector('.loader');
   const inputEl = document.getElementById('searchQuery');
@@ -12,38 +13,37 @@ export function fetchPics() {
   galleryEl.innerHTML = '';
   loaderEl.style.display = 'block';
 
-  const value = inputEl.value;
+  const value = inputEl.value.trim();
   inputEl.value = '';
-  const encodedValue = encodeURIComponent(value);
-  const params = new URLSearchParams({
-    key: API_KEY,
-    q: encodedValue,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-  });
-  const queryString = params.toString();
 
-  fetch(`https://pixabay.com/api/?${queryString}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
-    .then(data => {
-      if (data.hits.length === 0) {
-        iziToast.error({
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-        });
-
-        return;
-      }
-      renderPics(data);
-    })
-    .catch(error => console.log(error))
-    .finally(() => {
-      loaderEl.style.display = 'none';
+  try {
+    const response = await axios.get('https://pixabay.com/api', {
+      params: {
+        key: API_KEY,
+        q: value,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
     });
+
+    const data = response.data;
+
+    if (!data.hits.length) {
+      iziToast.error({
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+      });
+      return;
+    }
+
+    renderPics(data);
+  } catch (error) {
+    iziToast.error({
+      message: error.message,
+    });
+    console.error(error);
+  } finally {
+    loaderEl.style.display = 'none';
+  }
 }
